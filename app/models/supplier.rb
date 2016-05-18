c3db156284f3a20e3de9f225b101b1570544c35a
Carlos Arcenas <c.arcenas@me.com>
2016-05-18 14:26:37 +0000

    Finish tests for supplier_name.

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,3 @@
 class Supplier < ActiveRecord::Base
+    validates :supplier_name, presence: true, length: { maximum: 100 }
 end
--- test/models/supplier_test.rb
+++ test/models/supplier_test.rb
@@ -2,10 +2,21 @@ require 'test_helper'
 
 class SupplierTest < ActiveSupport::TestCase
   def setup
-    @supplier = Supplier(supplier_name: "ACME Inc.")
+    @supplier = Supplier.new(supplier_name: "ACME Inc.")
   end
   
   test "should be valid" do
     assert @supplier.valid?
   end
+  
+  test "supplier name should be present" do
+    @supplier.supplier_name = "      "
+    assert_not @supplier.valid?
+  end
+  
+  test "supplier name shouldn't be too long" do
+    @supplier.supplier_name = "a" * 101
+    assert_not @supplier.valid?
+  end
+  
 end
