3abcc9aeea7cd4534c8e208ab21f43947da7ad07
Carlos Arcenas <c.arcenas@me.com>
2016-05-18 12:31:06 +0000

    Created index for Managers.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518102729) do
+ActiveRecord::Schema.define(version: 20160518122821) do
 
   create_table "managers", force: :cascade do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20160518102729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "managers", ["email"], name: "index_managers_on_email", unique: true
 
 end
