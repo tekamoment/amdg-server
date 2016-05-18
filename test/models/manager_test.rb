9bd235f50aa7ee03e9db2dafb75ed4ba83417b89
Carlos Arcenas <c.arcenas@me.com>
2016-05-18 11:21:59 +0000

    Updated Gemfile to include test helpers.

--- test/models/manager_test.rb
+++ test/models/manager_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class ManagerTest < ActiveSupport::TestCase
+  
+  def setup
+    @user = Manager.new(name: "Example User", email: "example.user@example.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+
   # test "the truth" do
   #   assert true
   # end
