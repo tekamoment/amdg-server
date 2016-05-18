5781c5f490b1575a3680356cbf33a4048210a485
Carlos Arcenas <c.arcenas@me.com>
2016-05-18 14:45:00 +0000

    Finsh basic tests for itemtype.

--- app/models/item_type.rb
+++ app/models/item_type.rb
@@ -1,2 +1,3 @@
 class ItemType < ActiveRecord::Base
+    validates :itemtype, presence: true, length: { maximum: 50 }
 end
--- test/models/item_type_test.rb
+++ test/models/item_type_test.rb
@@ -5,6 +5,20 @@ class ItemTypeTest < ActiveSupport::TestCase
     @itemtype = ItemType.new(itemtype: "Camera")
   end
   
+  test "should be valid" do
+    assert @itemtype.valid?
+  end
+  
+  test "itemtype should be present" do
+    @itemtype.itemtype = "     "
+    assert_not @itemtype.valid?
+  end
+  
+  test "itemtype name shouldn't be too long" do
+    @itemtype.itemtype = "a" * 51
+    assert_not @itemtype.valid?
+  end
+  
   # test "the truth" do
   #   assert true
   # end
