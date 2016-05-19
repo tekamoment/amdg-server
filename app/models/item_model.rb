ec3a2e297af18de42638b1e0b11f3def10f44ee0
Carlos Arcenas <c.arcenas@me.com>
2016-05-19 04:46:39 +0000

    Add in basic tests for ItemModel.

--- app/models/item_model.rb
+++ app/models/item_model.rb
@@ -1,2 +1,6 @@
 class ItemModel < ActiveRecord::Base
+    validates :description, presence: true, length: { maximum: 100 }
+    validates :srp, presence: true, numericality: { greater_than: 0 }
+    validates :cost, presence: true, numericality: { greater_than: 0 }
+    validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- test/models/item_model_test.rb
+++ test/models/item_model_test.rb
@@ -1,6 +1,63 @@
 require 'test_helper'
 
 class ItemModelTest < ActiveSupport::TestCase
+  def setup
+    @model = ItemModel.new(description: "Canon EOS 7D", srp: 50000.00, quantity: 1, cost: 40000.00)
+  end
+  
+  test "should be valid" do
+    assert @model.valid?
+  end
+  
+  test "description should be present" do
+    @model.description = "    "
+    assert_not @model.valid?
+  end
+  
+  test "description should not be too long" do
+    @model.description = "a" * 101
+    assert_not @model.valid?
+  end
+  
+  test "srp should be present" do 
+    @model.srp = nil
+    assert_not @model.valid?
+  end
+  
+  test "srp should not be 0" do
+    @model.srp = 0
+    assert_not @model.valid?
+  end
+  
+  test "srp should not be negative" do
+    @model.srp = -1
+    assert_not @model.valid?
+  end
+  
+  test "cost should be present" do 
+    @model.cost = nil
+    assert_not @model.valid?
+  end
+  
+  test "cost should not be 0" do
+    @model.cost = 0
+    assert_not @model.valid?
+  end
+  
+  test "cost should not be negative" do
+    @model.cost = -1
+    assert_not @model.valid?
+  end
+  
+  test "quantity should be present" do 
+    @model.quantity = nil
+    assert_not @model.valid?
+  end
+  
+  test "quantity should not be negative" do
+    @model.quantity = -1
+    assert_not @model.valid?
+  end
   # test "the truth" do
   #   assert true
   # end
