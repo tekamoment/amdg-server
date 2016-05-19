a3169512e80d2298a2e2d9766534761646fbf0cc
Carlos Arcenas <c.arcenas@me.com>
2016-05-19 04:13:03 +0000

    Add in basic test for client_name.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,3 @@
 class Client < ActiveRecord::Base
+    validates :client_name, presence: true, length: { maximum: 50 }
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @client = Client.new(client_name: "ADMU")
+  end
+  
+  test "should be valid" do
+    assert @client.valid?
+  end
+  
+  test "client name should be present" do
+    @client.client_name = "    "
+    assert_not @client.valid?
+  end
+  
+  test "first name shouldn't be too long" do
+    @client.client_name = "a" * 51
+    assert_not @client.valid?
+  end
+  
 end
