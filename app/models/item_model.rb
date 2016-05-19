01c51e96ec492e18c9f5072a153e19884ba3ba8b
Carlos Arcenas <c.arcenas@me.com>
2016-05-19 21:06:07 +0000

    Data dictionary constraints on Item model.

--- app/models/item_model.rb
+++ app/models/item_model.rb
@@ -12,9 +12,9 @@ class ItemModel < ActiveRecord::Base
     has_many :transfer_itemlists
     has_many :transfers, :through => :transfer_itemlists
     
-    validates :description, presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
-    validates :srp, presence: true, numericality: { greater_than: 0 }
-    validates :cost, presence: true, numericality: { greater_than: 0 }
-    validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :description, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+    validates :srp, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1000000000 }
+    validates :cost, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1000000000 }
+    validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1000000}
     validates :item_type_id, presence: true
 end
