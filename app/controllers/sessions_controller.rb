5009a4849e359a189336f1bd0a48ec485a581ff2
Carlos Arcenas <c.arcenas@me.com>
2016-05-19 12:35:54 +0000

    Controller handles invalid submissions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,27 @@ class SessionsController < ApplicationController
   
   def new
   end
+  
+  def create
+    temp_user = Manager.find_by(email: params[:session][:email].downcase)
+    if !temp_user
+      temp_user = Agent.find_by(email: params[:session][:email].downcase)
+      if !temp_user
+        temp_user = WarehouseStaffer.find_by(email: params[:session][:email].downcase)
+      end
+    end
+    
+    user = temp_user
+    if user && user.authenticate(params[:session][:password])
+      # Log in!
+    else
+      puts 'login unsuccessful'
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
+  
 end
