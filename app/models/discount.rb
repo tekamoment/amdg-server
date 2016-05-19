276ccdd9a383b2da4f5c30fced48b605d39872c1
Carlos Arcenas <c.arcenas@me.com>
2016-05-19 05:03:18 +0000

    Finished tests for discount_rate.

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,2 +1,4 @@
 class Discount < ActiveRecord::Base
+    validates :discount_rate, presence: true, numericality: { greater_than: 0, less_than: 1 }
 end
+
--- test/models/discount_test.rb
+++ test/models/discount_test.rb
@@ -4,4 +4,32 @@ class DiscountTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @model = Discount.new(discount_rate: 0.5)
+  end
+  
+  test "should be valid" do
+    assert @model.valid?
+  end
+  
+   test "discount_rate should be present" do 
+    @model.discount_rate = nil
+    assert_not @model.valid?
+  end
+  
+  test "discount_rate should not be 0" do
+    @model.discount_rate = 0
+    assert_not @model.valid?
+  end
+  
+  test "discount_rate should not be negative" do
+    @model.discount_rate = -1
+    assert_not @model.valid?
+  end
+  
+  test "discount_rate should not be greater than 1" do
+    @model.discount_rate = 1
+    assert_not @model.valid?
+  end
 end
